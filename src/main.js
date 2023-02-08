4438ae3cd34adbef8c5e5f6ebfa2957a24011cad
ecj222 <enochchejieh@gmail.com>
2023-02-08 18:41:03 +0100

    updated main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import axios from "axios";
-import VueAxios from "vue-axios";
+import store from "./store";
 
-const app = createApp(App).mount("#app");
+const app = createApp(App);
 
-app.use(VueAxios, axios);
+app.use(store);
+
+app.mount("#app");
