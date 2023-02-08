8f49a7ff9d04e67f2c99f257f88db0f051eca1b5
ecj222 <enochchejieh@gmail.com>
2023-02-08 18:39:41 +0100

    created dog vuex module

--- src/store/modules/dogs.js
+++ src/store/modules/dogs.js
@@ -1,10 +1,29 @@
-const state = () => ({});
+import { http } from "@/utils/axios";
 
-const getters = {};
+const state = () => ({
+  dog_breeds: [],
+});
 
-const actions = {};
+const getters = {
+  dog_breeds: (state) => state.dog_breeds,
+};
 
-const mutations = {};
+const actions = {
+  async getDogs({ commit }) {
+    try {
+      const response = await http.get("https://dog.ceo/api/breeds/list/all");
+      commit("SET_DOGS_BREEDS", response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  },
+};
+
+const mutations = {
+  SET_DOGS_BREEDS: (state, data) => {
+    state.dog_breeds = data;
+  },
+};
 
 export default {
   namespaced: true,
