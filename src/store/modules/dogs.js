ab5abf64b851012520cee4268406c7462f623191
ecj222 <enochchejieh@gmail.com>
2023-02-08 22:07:56 +0100

    added integration for dog images

--- src/store/modules/dogs.js
+++ src/store/modules/dogs.js
@@ -1,5 +1,10 @@
 import { http } from "@/utils/axios";
 
+const getUniqueDogBreeds = (current_breeds, data) => {
+  const breedsSet = new Set(current_breeds);
+  return data.filter((dogs_images) => !breedsSet.has(dogs_images));
+};
+
 const state = () => ({
   dog_breeds: [],
 });
@@ -11,8 +16,16 @@ const getters = {
 const actions = {
   async getDogs({ commit }) {
     try {
-      const response = await http.get("https://dog.ceo/api/breeds/list/all");
-      commit("SET_DOGS_BREEDS", response.data);
+      const breeds = [];
+      let count = 0;
+      while (count < 100) {
+        const response = await http.get("/breeds/image/random/50");
+        const uniqueBreeds = getUniqueDogBreeds(breeds, response.data.message);
+        const data = uniqueBreeds.slice(0, 100 - count);
+        breeds.push(...data);
+        count += data.length;
+      }
+      commit("SET_DOG_BREEDS", breeds);
     } catch (error) {
       console.error(error);
     }
@@ -20,7 +33,7 @@ const actions = {
 };
 
 const mutations = {
-  SET_DOGS_BREEDS: (state, data) => {
+  SET_DOG_BREEDS(state, data) {
     state.dog_breeds = data;
   },
 };
