130f3a826f27a1647e1da11824e9ce7292f97841
ecj222 <enochchejieh@gmail.com>
2023-02-09 17:35:55 +0100

    add intersectionObserver api and refactor

--- src/App.vue
+++ src/App.vue
@@ -1,103 +1,73 @@
 <template>
   <div class="container">
-    <h1 class="header">Looking for a dog 🐾</h1>
-    <div class="search">
-      <input
-        type="search"
-        placeholder="Have one in mind?"
-        v-model="search_text"
-        @input.prevent="search"
-      />
-    </div>
-    <dog-grid :dog-breeds="dogs" />
-    <dog-loader v-show="loading" />
-    <button
-      v-show="!loading && dogs.length < 148 && search_text.trim().length < 1"
-      class="load-more"
-      @click="loadMoreImages"
-    >
-      Load more
-    </button>
+    <Logo />
+    <router-view />
   </div>
 </template>
 
 <script>
-import DogGrid from "@/components/DogGrid.vue";
-import DogLoader from "@/components/DogLoader.vue";
-import { debounce } from "@/utils/fn";
-import { mapGetters } from "vuex";
-
+import Logo from "@/components/Logo.vue";
 export default {
-  name: "DogList",
-  data() {
-    return {
-      search_text: "",
-    };
-  },
   components: {
-    DogGrid,
-    DogLoader,
+    Logo,
   },
-  computed: {
-    ...mapGetters("dogs", ["dogs", "loading"]),
+  mounted() {
+    window.addEventListener("load", this.lazyLoadImages);
+    window.addEventListener("scroll", this.lazyLoadImages);
+    window.addEventListener("resize", this.lazyLoadImages);
+    window.addEventListener("orientationChange", this.lazyLoadImages);
+    window.addEventListener("beforeunload", this.resetState());
   },
-  created() {
-    this.debouncedSearch = debounce((search_text) => {
-      this.$store.dispatch("dogs/searchDog", search_text);
-    }, 2000);
-    this.$store.dispatch("dogs/getBreedList");
-    this.$store.dispatch("dogs/getDogs");
+  beforeUnmount() {
+    window.removeEventListener("load", this.lazyLoadImages);
+    window.removeEventListener("scroll", this.lazyLoadImages);
+    window.removeEventListener("resize", this.lazyLoadImages);
+    window.removeEventListener("orientationChange", this.lazyLoadImages);
+    window.removeEventListener("beforeunload", this.resetState());
   },
   methods: {
-    loadMoreImages() {
-      this.$store.dispatch("dogs/loadMoreDogs");
+    preloadImages(image) {
+      const src = image.getAttribute("data-src");
+      if (!src) {
+        return;
+      }
+      image.src = src;
+      image.style.visibility = "visible";
     },
-    search() {
-      this.debouncedSearch(this.search_text);
+    lazyLoadImages() {
+      const images = document.querySelectorAll("[data-src]");
+
+      const options = {
+        threshold: 1,
+        rootMargin: "0px 0px 50px 0px",
+      };
+
+      const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            const image = entry.target;
+            this.preloadImages(image);
+            observer.unobserve(image);
+          } else {
+            return;
+          }
+        });
+      }, options);
+
+      images.forEach((image) => {
+        observer.observe(image);
+      });
+    },
+    resetState() {
+      this.$store.commit("SET_DOGS", this.$store.state.initial_data);
     },
   },
 };
 </script>
 
-<style scoped>
+<style>
 .container {
   width: 100vw;
-}
-.container .header {
-  text-align: center;
-  font-size: 5rem;
-  color: #484848;
-  margin: 3rem 0 4rem;
-}
-.container button.load-more {
-  background-color: #484848;
-  border: none;
-  border-radius: 5px;
-  color: #fff;
-  font-size: 1.5rem;
-  padding: 1rem 2rem;
-  text-align: center;
-  display: block;
-  margin: 3rem auto;
-  cursor: pointer;
-}
-.search {
-  margin: 0 auto 3rem;
-  text-align: center;
-}
-.search input[type="search"] {
-  width: 50%;
-  padding: 1rem 2rem;
-  font-size: 1.5rem;
-  border-radius: 10px;
-  border: 1px solid #484848;
-  outline: none;
-  box-shadow: 0px 0px 10px 0px #ccc;
-  transition: all 0.3s;
-}
-
-.search input[type="search"]:focus {
-  width: 60%;
-  box-shadow: 0px 0px 10px 0px #484848;
+  padding: 1rem;
 }
 </style>
